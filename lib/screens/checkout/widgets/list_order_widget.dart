5d363fbc87ec9d25f7639932574648e77d131e9b
Pablo Sanjuan Muñoz <pabsanmun@alu.edu.gva.es>
2023-06-10 17:54:13 +0200

    If order empty display meesage

--- lib/screens/checkout/widgets/list_order_widget.dart
+++ lib/screens/checkout/widgets/list_order_widget.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:quick_order/provider/products_provider.dart';
-import 'package:quick_order/provider/user_provider.dart';
 import 'package:quick_order/screens/checkout/widgets/modify_qty_widget.dart';
-import 'package:quick_order/screens/checkout/widgets/order_line_cart_widget.dart';
-import 'package:quick_order/screens/profile/payment/widget/payment_cart_widget.dart';
-
-import '../../../models/order_line.dart';
+import '../../home/search_screen.dart';
+import 'order_line_cart_widget.dart';
 
 class ListOrderWidget extends StatelessWidget {
-  const ListOrderWidget({super.key, required this.provider});
+  const ListOrderWidget({Key? key, required this.provider}) : super(key: key);
   final ProductsProvider provider;
 
   @override
@@ -16,26 +13,37 @@ class ListOrderWidget extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return ConstrainedBox(
       constraints: BoxConstraints(maxHeight: size.height - 60),
-      child: ListView.builder(
-        itemCount: provider.order.lines.length,
-        scrollDirection: Axis.vertical,
-        itemBuilder: (_, index) {
-          return InkWell(
-            onTap: () {
-              showModalBottomSheet(
-                context: context,
-                backgroundColor: Colors.transparent,
-                builder: (context) {
-                  return ModifyQty(provider: provider, index: index,);
-                },
-              );
-            },
-            borderRadius: BorderRadius.circular(25),
-            child: OrderLineCardWidget(
-             orderLine: provider.order.lines[index],
+      child: Column(
+        children: [
+          if (provider.order.lines.isEmpty)
+            const NotFoundDataWidget(
+              message: "No items in your order",
             ),
-          );
-        },
+          if (provider.order.lines.isNotEmpty)
+            Expanded(
+              child: ListView.builder(
+                itemCount: provider.order.lines.length,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (_, index) {
+                  return InkWell(
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        backgroundColor: Colors.transparent,
+                        builder: (context) {
+                          return ModifyQty(provider: provider, index: index);
+                        },
+                      );
+                    },
+                    borderRadius: BorderRadius.circular(25),
+                    child: OrderLineCardWidget(
+                      orderLine: provider.order.lines[index],
+                    ),
+                  );
+                },
+              ),
+            ),
+        ],
       ),
     );
   }
